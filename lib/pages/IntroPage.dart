d4bc404d26f774fd47a0ac019e2b40d32f151696
Demo <pandeysatyam9833@gmail.com>
2025-07-10 23:46:40 +0530

    Adding HomePage

--- lib/pages/IntroPage.dart
+++ lib/pages/IntroPage.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:sneakers_shop_app/pages/Home_Page.dart';
 
 class Intropage extends StatelessWidget {
   const Intropage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/Images/nike-logo.png', height: 250),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Just Do It",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Brand new Sneakers and Custom Kicks with premium quality",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                  color: Colors.grey[500],
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Center(
+                      child: Text(
+                        "Lets Shop now",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
