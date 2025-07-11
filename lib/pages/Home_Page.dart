f150232cafae64710fad10a1ef0b9f3fb0105935
Demo <pandeysatyam9833@gmail.com>
2025-07-11 23:02:46 +0530

    Bottom Navigation bar

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sneakers_shop_app/components/botttom_nav_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void navigatebottombar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyButtomNavBar(
+        onTabChange: (index) => navigatebottombar(index),
+      ),
+    );
   }
 }
