021d8454a4d9941b99f649424ca564fbcda7e6e0
Demo <pandeysatyam9833@gmail.com>
2025-07-12 23:05:15 +0530

    Routing in between home page cart page

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sneakers_shop_app/components/botttom_nav_bar.dart';
+import 'package:sneakers_shop_app/pages/cart_page.dart';
+import 'package:sneakers_shop_app/pages/shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +18,8 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  final List<Widget> _pages = [const ShopPage(), const CartPage()];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +27,14 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: MyButtomNavBar(
         onTabChange: (index) => navigatebottombar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu, color: Colors.black),
+        ),
+      ),
+      body: _pages[_selectedIndex],
     );
   }
 }
